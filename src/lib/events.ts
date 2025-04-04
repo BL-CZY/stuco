c7eaf0bc2c3f7af9a0ae0f8ef2ee3891f12584ea
BL-CZY <jiguangllsw@gmail.com>
2025-04-04 18:18:08 +0200

    refactor event signup form structure and enhance dynamic field addition

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,28 +1,60 @@
+export interface EventSignupElementText {
+    type: 'text';
+    text: string;
+    conditions: { name: string; target: string; negate: boolean }[];
+    color: 'regular' | 'warning';
+}
+
+export interface EventSignupElementQuestion {
+    type: 'question';
+    // conditions under which this question will show
+    conditions: { name: string; target: string; negate: boolean }[];
+    name: string;
+    body:
+        | {
+              type: 'checkbox';
+              // bind to one of the booleans
+              bind: string;
+          }
+        | {
+              type: 'input';
+          }
+        | {
+              type: 'select';
+              options: string[];
+          };
+}
+
+export interface EventSignupQuestionCheckbox {
+    type: 'checkbox';
+    // bind to one of the booleans
+    bind: string;
+}
+
+export interface EventSignupQuestionInput {
+    type: 'input';
+}
+
+export interface EventSignupQuestionSelect {
+    type: 'select';
+    options: string[];
+}
+
+export type EventSignupQuestionBody =
+    | EventSignupQuestionCheckbox
+    | EventSignupQuestionInput
+    | EventSignupQuestionSelect;
+
+export interface EventSignupQuestion {
+    type: 'question';
+    // conditions under which this question will show
+    conditions: { name: string; target: string; negate: boolean }[];
+    name: string;
+    body: EventSignupQuestionBody;
+}
+
+export type EventSignupElement = EventSignupElementText | EventSignupElementQuestion;
+
 export interface EventSignupForm {
-    elements: (
-        | {
-              type: 'text';
-              text: string;
-              color: 'regular' | 'warning';
-          }
-        | {
-              type: 'question';
-              // conditions under which this question will show
-              conditions: { name: string; target: string; negate: boolean }[];
-              name: string;
-              body:
-                  | {
-                        type: 'checkbox';
-                        // bind to one of the booleans
-                        bind: string;
-                    }
-                  | {
-                        type: 'input';
-                    }
-                  | {
-                        type: 'select';
-                        options: string[];
-                    };
-          }
-    )[];
+    elements: EventSignupElement[];
 }
--- src/routes/private/add-event/+page.svelte
+++ src/routes/private/add-event/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { EventSignupForm } from '$lib/events';
 
+    let fieldType = $state('');
     let form: EventSignupForm = $state({ booleans: [], elements: [] });
 </script>
 
@@ -9,4 +10,55 @@
 <form action="?/new-event">
     <input type="text" name="eventName" id="eventName" placeholder="Event Name" required />
     <input type="file" name="flyer" id="flyer" accept="image/*" />
+    {#each form.elements as element, i}
+        {#if element.type === 'question'}
+            <div>
+                <label for={`${i}-q-selection`}>Question Type</label>
+                <select
+                    name={`${i}-q-selection`}
+                    id={`${i}-q-selection`}
+                    bind:value={element.body.type}
+                >
+                    <option value="checkbox">Checkbox</option>
+                    <option value="input">Input</option>
+                    <option value="select">Selection</option>
+                </select>
+
+                <p>{element.name}</p>
+                <p>{element.conditions}</p>
+            </div>
+        {:else if element.type === 'text'}
+            <div>
+                <p>{element.text}</p>
+                <p>{element.color}</p>
+                <p>{element.conditions}</p>
+            </div>
+        {/if}
+    {/each}
+    <select name="fieldType" id="fieldType" bind:value={fieldType}>
+        <option value="text">Text</option>
+        <option value="question">Question</option>
+    </select>
+    <button
+        onclick={() => {
+            if (fieldType === 'text') {
+                form.elements.push({
+                    type: 'text',
+                    text: '',
+                    color: 'regular',
+                    conditions: []
+                });
+            } else if (fieldType === 'question') {
+                form.elements.push({
+                    type: 'question',
+                    name: '',
+                    conditions: [],
+                    body: {
+                        type: 'input'
+                    }
+                });
+            }
+        }}
+        type="button">New field</button
+    >
 </form>
